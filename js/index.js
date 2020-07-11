d4ffd048a2858c80c8a58f1b566834d29f8cc76a
harvey magana <heyharv2012@gmail.com>
2020-07-11 09:34:44 -0700

    Added helpful comments to the code blocks to identify what they are and what they do.

--- js/index.js
+++ js/index.js
@@ -19,6 +19,7 @@ let inverseImg= document.querySelector('.inverse-section .img-content');
 let bottomImg = document.querySelector('.content-destination img');
 let destinations = document.querySelectorAll('.destination');
 
+//change the header logo to green when the page loads (1)
 window.addEventListener('load', () => {
     if(event.target === 'load') {
         headerLogo.style.color = "yellow";
@@ -27,26 +28,30 @@ window.addEventListener('load', () => {
     }
 })
 
+//change the text in the paragragh to yellow when you key down (2)
 document.addEventListener('keydown', () => {
     headerP.style.color = "yellow";
 });
-
+//change the text in the paragragh back to black when you key up (3)
 document.addEventListener('keyup', () => {
     headerP.style.color = "";
 });
 
+//show the border when you hover over (4)
 heroImg.addEventListener('mouseover', () => {
     heroImg.style.border = "3px solid pink";
 })
-
+//remove the border when you hover away (5)
 heroImg.addEventListener('mouseout', () => {
     heroImg.style.border = "";
 })
 
+//change the h2 to show the pageXOffset with a scroll (5)
 window.addEventListener('scroll', function() {
     inverseContent.lastElementChild.firstElementChild.textContent = window.pageXOffset + 'px';
 });
 
+//provide MousEvent details with a right-click (6)
 (function() {
     var taskItems = document.querySelectorAll(".nav");
   
@@ -61,16 +66,19 @@ window.addEventListener('scroll', function() {
     }
   })();
 
+//change the text in the middle button with a click (7)
 destinations[1].lastElementChild.addEventListener('click', () => {
     destinations[1].lastElementChild.textContent = 'Nevermind...';
 });
 
+//reverse the bottom image with a double click (8)
 bottomImg.addEventListener('dblclick', () => {
     if (event.type === 'dblclick') {
         bottomImg.style.transform = "scaleX(-1)"; 
     } 
 })
 
+//nav drag event (9)
 navLink[0].setAttribute('draggable', true);
 navLink[0].setAttribute('ondragstart', "event.dataTransfer.setData('text/plain',null)");
 
@@ -79,6 +87,7 @@ document.addEventListener("drag", function(event) {
 
 }, false);
 
+//drag event on the navigation (10)
 document.addEventListener("dragstart", function(event) {
     // store a ref. on the dragged elem
     dragged = event.target;
