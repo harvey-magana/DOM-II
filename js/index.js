2fe8e786cdd0ecd1a565811518f20662f04d4cd9
harvey magana <heyharv2012@gmail.com>
2020-07-11 09:21:25 -0700

    Added drag event to the Home navigation element.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 let headerLogo = document.querySelector('.logo-heading');
 let nav = document.querySelectorAll('.nav');
+let navLink = document.querySelectorAll('.nav-link');
 
 let heroImg = document.querySelector('.intro img');
 
@@ -69,3 +70,56 @@ bottomImg.addEventListener('dblclick', () => {
         bottomImg.style.transform = "scaleX(-1)"; 
     } 
 })
+
+navLink[0].setAttribute('draggable', true);
+navLink[0].setAttribute('ondragstart', "event.dataTransfer.setData('text/plain',null)");
+
+let dragged;
+document.addEventListener("drag", function(event) {
+
+}, false);
+
+document.addEventListener("dragstart", function(event) {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function(event) {
+    // reset the transparency
+    event.target.style.opacity = "";
+}, false);
+
+document.addEventListener("dragover", function(event) {
+    // prevent default to allow drop
+    event.preventDefault();
+  }, false);
+
+  document.addEventListener("dragenter", function(event) {
+    // highlight potential drop target when the draggable element enters it
+    if (event.target.className == "nav-link") {
+      event.target.style.background = "grey";
+      event.target.style.margin = "0 1.6rem";
+    }
+  
+  }, false);
+
+  document.addEventListener("dragleave", function(event) {
+    // reset background of potential drop target when the draggable element leaves it
+    if (event.target.className == "nav-link") {
+      event.target.style.background = "";
+    }
+  
+  }, false);
+
+  document.addEventListener("drop", function(event) {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    // move dragged elem to the selected drop target
+    if (event.target.className == "nav-link") {
+      event.target.style.background = "";
+      dragged.parentNode.removeChild( dragged );
+      event.target.appendChild( dragged );
+    }
+  }, false);
