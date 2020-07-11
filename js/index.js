335e651b340268b921db45a0df38bf7aeca2111b
harvey magana <heyharv2012@gmail.com>
2020-07-10 17:56:00 -0700

    Created some variables from some DOM elements.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+let headerLogo = document.querySelector('.logo-heading');
+let nav = document.querySelectorAll('.nav');
+
+let heroImg = document.querySelector('.intro img');
+
+let headerP = document.querySelector('.intro p');
+let letters = Array.from(headerP.textContent);
+
+let contentSection = document.querySelector('.content-section');
+let contentText = document.querySelector('.content-section .text-content');
+let contentImg = document.querySelector('.content-section .img-content');
+
+let inverseContent = document.querySelector('.inverse-section');
+let inverseText = document.querySelector('.inverse-section .text-content');
+let inverseImg= document.querySelector('.inverse-section .img-content');
+
+
+  
