ae319b6d5ddd619ad1e4a3327ed9b4eb0174ed9e
harvey magana <heyharv2012@gmail.com>
2020-07-13 17:43:16 -0700

    Made it so that the block moves to the right continuously.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -19,3 +19,33 @@ window.addEventListener('click', () => {
         }
     }
 })
+
+window.addEventListener('mousedown', () => {
+    for (let [j] of blocksArr.entries()) {
+        if(event.target === blocksArr[j] && event.type === 'mousedown') {
+            /*
+            blocksArr[j].style.animationDuration = "3s";
+            blocksArr[j].style.animationTimingFunction = "linear";
+            blocksArr[j].style.animationDelay = "1s";
+            blocksArr[j].style.animationIterationCount = "infinite";
+            blocksArr[j].style.animationPlayState = "running";
+            blocksArr[j].style.animationPlayState = "slidein";
+            blocksArr[j].style.animation = "linear 1s infinite running slidein";
+            */
+           blocksArr[j].style.position = "relative";
+           blocksArr[j].style.cursor = "pointer";
+
+           let start = Date.now();
+
+           let timer = setInterval(function() {
+             let timePassed = Date.now() - start;
+     
+             blocksArr[j].style.left = timePassed / 5 + 'px';
+     
+             if (timePassed > 9000) clearInterval(timer);
+     
+           }, 20);
+            console.log("Hello")
+        }
+    }
+})
